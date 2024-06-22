4d8173aba7200a084424da449fafd0c780ff757a
Mohammed <elamrani.mohammed95@gmail.com>
2024-06-22 23:54:51 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.frontend',
+  appName: 'frontend',
+  webDir: 'dist'
+};
+
+export default config;
