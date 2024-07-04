d1153f191fef3c82723a7ea6b64f741adabd853f
Mohammed <elamrani.mohammed95@gmail.com>
2024-07-04 22:39:45 +0200

    update fichier store PINIA , ajout de localstorage token + user info  + abonnement, gestion du logout + abonnement ( finalement accessible meme si non connected )

---
+++ src/store/store.js
@@ -0,0 +1,60 @@
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import apiClient from '@/axios';
+
+export const useAuthStore = defineStore('auth', () => {
+  const user = ref(null);
+  const token = ref(localStorage.getItem('token') || ''); 
+  const subscriptions = ref([]);
+
+  const setUser = (userData) => {
+    user.value = userData;
+    localStorage.setItem('user', JSON.stringify(userData)); 
+  };
+
+  const setToken = (tokenData) => {
+    token.value = tokenData;
+    localStorage.setItem('token', tokenData); 
+  };
+
+  const setSubscriptions = (subscriptionData) => {
+    subscriptions.value = subscriptionData;
+  };
+
+  const fetchSubscriptions = async () => {
+    try {
+      const response = await apiClient.get('/users/subscriptions', {
+        headers: {
+          Authorization: `Bearer ${token.value}`
+        }
+      });
+      subscriptions.value = response.data;
+    } catch (error) {
+      console.error('Erreur lors de la récupération des abonnements:', error.response.data);
+    }
+  };
+
+  const logout = () => {
+    user.value = null;
+    token.value = '';
+    subscriptions.value = [];
+    localStorage.removeItem('user'); 
+    localStorage.removeItem('token'); 
+  };
+
+  // Initialisation
+  if (localStorage.getItem('user')) {
+    user.value = JSON.parse(localStorage.getItem('user'));
+  }
+
+  return {
+    user,
+    token,
+    subscriptions,
+    setUser,
+    setToken,
+    setSubscriptions,
+    fetchSubscriptions,
+    logout
+  };
+});
