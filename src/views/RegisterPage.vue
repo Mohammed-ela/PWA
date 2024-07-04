e2e0ac578250eba946a29dc45b56ca6d90a653a1
Mohammed <elamrani.mohammed95@gmail.com>
2024-07-04 23:01:29 +0200

    update page inscription avec API

--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -24,19 +24,43 @@
         </ion-item>
       </ion-list>
       <ion-button expand="full" @click="register">S'inscrire</ion-button>
+      <ion-item router-link="/login" detail="false">
+        <ion-icon slot="start" name="log-in-outline"></ion-icon>
+        <ion-label>Vous êtes déjà inscrit ?</ion-label>
+      </ion-item>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup>
 import { ref } from 'vue';
-import { IonButton, IonContent, IonHeader, IonInput, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar, IonButtons, IonMenuButton } from '@ionic/vue';
+import { useRouter } from 'vue-router';
+import { useAuthStore } from '@/store/store';
+import apiClient from '@/axios';
+import {
+  IonButton, IonContent, IonHeader, IonInput, IonItem, IonLabel,
+  IonList, IonPage, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonIcon
+} from '@ionic/vue';
+import { logInOutline } from 'ionicons/icons';
 
 const name = ref('');
 const email = ref('');
 const password = ref('');
+const router = useRouter();
+const authStore = useAuthStore();
 
-const register = () => {
-  // Ajoutez la logique d'inscription ici
+const register = async () => {
+  try {
+    const response = await apiClient.post('/users/inscription', {
+      name: name.value,
+      email: email.value,
+      password: password.value
+    });
+    authStore.setUser(response.data.user);
+    authStore.setToken(response.data.token);
+    router.push('/login'); 
+  } catch (error) {
+    console.error('Erreur d\'inscription:', error.response.data);
+  }
 };
 </script>
