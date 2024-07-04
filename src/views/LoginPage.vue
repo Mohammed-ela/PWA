2bb660ba2ab64fded50c303b11bbcaa1c8eab2e1
Mohammed <elamrani.mohammed95@gmail.com>
2024-07-04 22:50:36 +0200

    Login page => Pinia pour update le localstorage si  success login / Axios API pour l'appel de l'endpoint login

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -20,18 +20,37 @@
         </ion-item>
       </ion-list>
       <ion-button expand="full" @click="login">Se connecter</ion-button>
+      <ion-item router-link="/register" detail="false">
+        <ion-icon slot="start" name="person-add-outline"></ion-icon>
+        <ion-label>Pas encore inscrit ?</ion-label>
+      </ion-item>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup>
 import { ref } from 'vue';
-import { IonButton, IonContent, IonHeader, IonInput, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar, IonButtons, IonMenuButton } from '@ionic/vue';
+import { useRouter } from 'vue-router';
+import { useAuthStore } from '@/store/store';
+import apiClient from '@/axios';
+import { IonButton, IonContent, IonHeader, IonInput, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonIcon } from '@ionic/vue';
 
 const email = ref('');
 const password = ref('');
+const router = useRouter();
+const authStore = useAuthStore();
 
-const login = () => {
-  // Ajoutez la logique de connexion ici
+const login = async () => {
+  try {
+    const response = await apiClient.post('/users/connexion', {
+      email: email.value,
+      password: password.value
+    });
+    authStore.setUser(response.data.existingUser);
+    authStore.setToken(response.data.token);
+    router.push('/');
+  } catch (error) {
+    console.error('Erreur de connexion:', error.response.data);
+  }
 };
 </script>
